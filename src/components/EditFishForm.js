444649cc3e877b473c0279fe430535677170e4a1
Luke Logan <llogan382@gmail.com>
2019-10-31 15:43:27 -0400

    Create a form that takes in data to update state

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 
 class EditFishForm extends React.Component {
-    render() {
-        return <p>Edit Fish</p>
+    handleChange = (event) => {
+        console.log(event.currentTarget.value);
+
+        // Update the object. Here we are spreading the props and assigning them to updatedFish
+        // This is send UPSTREAM into app, to update STATE with what is entered in the form
+        const updatedFish = {
+
+            // Take a copy of the fish, and overwrite what has changed.
+            //using spread, take the target NAME with the target VALUE
+            ...this.props.fish,
+            [event.currentTarget.name]: event.currentTarget.value
+        };
+        this.props.updateFish(this.props.index, updatedFish)
     }
+
+    render() {
+        return (
+            <div className="fish-edit">
+                <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
+                <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+                <select type="text" name="status" onChange={this.handleChange} value={this.props.fish.status} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+                <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image} />
+
+
+            </div>
+        )
+    };
 }
 
 export default EditFishForm;
