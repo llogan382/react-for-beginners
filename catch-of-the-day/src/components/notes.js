df684387717fd1717bb3c7adbc53b467fcf92031
Luke Logan <llogan382@gmail.com>
2020-01-30 06:13:49 -0500

    Notes

--- catch-of-the-day/src/components/notes.js
+++ catch-of-the-day/src/components/notes.js
@@ -8,3 +8,9 @@
 
 
 // Video 15: Use javascript to loop over items. Practice by viewing items in the console and viewing componentd
+
+// Video 18. Lifecycle methods. Load state from the firebase app after the app component is mounted.
+
+//use component lifecycle, componentdidmount
+
+// If going back and forth between components, the component is listening for changes in each of those components. This can cause a memory leak
